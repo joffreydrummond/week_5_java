2a9f3c957b0a13533fc275cf62db1d200eb6ce54
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-02-17 09:36:39 -0700

    implemented Logger interface on SpacedLogger class. implemented override methods

--- src/Interface/SpacedLogger.java
+++ src/Interface/SpacedLogger.java
@@ -1,4 +1,13 @@
 package Interface;
 
-public class SpacedLogger {
+public class SpacedLogger implements Logger{
+    @Override
+    public void log(String log) {
+
+    }
+
+    @Override
+    public void error(String err) {
+
+    }
 }
