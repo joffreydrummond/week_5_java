f46a0dfb97c5dd004507bf56218feed5405de2b4
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-02-17 09:48:29 -0700

    added date and functionality to log and error method in AsteriskLogger class

--- src/Interface/AsteriskLogger.java
+++ src/Interface/AsteriskLogger.java
@@ -1,13 +1,17 @@
 package Interface;
 
+import java.util.Date;
+
 public class AsteriskLogger implements Logger{
     @Override
     public void log(String log) {
-
+        Date date = new Date();
+        System.out.println(date + "\n************" + log + "************");
     }
 
     @Override
     public void error(String err) {
-
+        Date date = new Date();
+        System.out.println(date + "\n***************\n" + "***" + err +"***" + "\n************");
     }
 }
