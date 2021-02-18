adbdee97fb8f5747b9755530f4f0b0d437e42ccd
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-02-17 19:15:56 -0700

    too much time on hands changed look. 145678 times lol

--- src/Interface/AsteriskLogger.java
+++ src/Interface/AsteriskLogger.java
@@ -2,17 +2,20 @@ package Interface;
 
 import java.util.Date;
 
-public class AsteriskLogger implements Logger{
+public class AsteriskLogger implements Logger {
     @Override
     public void log(String log) {
         Date date = new Date();
+        System.out.println("------------------ASTERISK-----------------------");
         System.out.println(date + "\n************" + log + "************");
-        System.out.println("#################################################");
     }
 
     @Override
     public void error(String err) {
         Date date = new Date();
-        System.out.println(date + "\n*************\n" + "***" + err +"***" + "\n*************");
-        System.out.println("#################################################");    }
+        System.out.println(date + "\n**************************\n" + "***" + err + "***" + "\n**************************");
+        System.out.println("------------------ASTERISK-----------------------");
+        System.out.println("#################################################");
+    }
+
 }
--- src/Interface/SpacedLogger.java
+++ src/Interface/SpacedLogger.java
@@ -6,14 +6,16 @@ public class SpacedLogger implements Logger{
     @Override
     public void log(String log) {
         Date date = new Date();
+        System.out.println("==================SPACED=========================");
         System.out.println(date + "\n" + log.replace("", " ").trim());
-        System.out.println("#################################################");
+
     }
 
     @Override
     public void error(String err) {
         Date date = new Date();
         System.out.println(date + "\n" + "ERROR: " + err.replace("", " ").trim());
+        System.out.println("==================SPACED=========================");
         System.out.println("#################################################");
     }
 }
